f01046089b4f474a292cdf59963c2711e23994d5
Rajko Podinic <rajko.podinic@.com>
2021-08-26 17:01:23 +1000

    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout';
+import GoogleTagManager from '../components/GoogleTagManager'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <GoogleTagManager>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GoogleTagManager>
   )
 }
 
